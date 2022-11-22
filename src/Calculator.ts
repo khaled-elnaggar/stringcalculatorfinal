dcdb08969a54eb979e7ee6c57103bbd6e4dcdc51
Khaled Elnaggar <dev_khalid@hotmail.com>
2022-11-22 16:30:58 +0200

    Custom separator - 1st test AFTER refactor

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -2,33 +2,50 @@ import { InvalidInputError } from "./InvalidInputError";
 import { StringCalculatorGuard } from "./StringCalculatorGuard";
 
 export class Calculator {
+  public static readonly defaultSeparator = ',';
 
   public static add(numbersString: string): number {
     StringCalculatorGuard.checkForMissingNumberNewlineAndComma(numbersString);
     StringCalculatorGuard.checkForMissingNumberCommas(numbersString);
-    let separator: string = ',';
 
-    const customDelimiterPattern: RegExp = new RegExp("//.+\n");
-    const match = numbersString.match(customDelimiterPattern);
-
-    if(match){
-      separator = numbersString.substring(2, match[0].length);
-      numbersString = numbersString.substring(match[0].length);
-    }
+    let separator = Calculator.extractCustomSeparator(numbersString);
+    numbersString = Calculator.removeCustomSeparatorDefinition(numbersString);
 
     const separatorRegexPattern: RegExp = new RegExp(`[${separator}\n]`)
     const stringNumbers: string[] = numbersString.split(separatorRegexPattern);
 
     let sum: number = stringNumbers
-    .map(s => {
-      const num: number = Number(s);
-      StringCalculatorGuard.checkForNonNumber(num);
-      return num;
-    })
-    .reduce((a, b) => a + b, 0);
+      .map(s => {
+        const num: number = Number(s);
+        StringCalculatorGuard.checkForNonNumber(num);
+        return num;
+      })
+      .reduce((a, b) => a + b, 0);
 
     sum = Calculator.roundToOneDecimalPoint(sum);
     return sum;
+  }
+
+  private static removeCustomSeparatorDefinition(numbersString: string) {
+    const customDelimiterPattern: RegExp = new RegExp("//.+\n");
+    const match = numbersString.match(customDelimiterPattern);
+
+    if (match) {
+      numbersString = numbersString.substring(match[0].length);
+    }
+
+    return numbersString;
+  }
+
+  private static extractCustomSeparator(numbersString: string): string {
+    const customDelimiterPattern: RegExp = new RegExp("//.+\n");
+    const match = numbersString.match(customDelimiterPattern);
+
+    if (match) {
+      return numbersString.substring(2, match[0].length);
+    } else {
+      return this.defaultSeparator;
+    }
   }
 
   private static roundToOneDecimalPoint(sum: number) {
